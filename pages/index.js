5a002e8f261bc506bf67df467ac3d406ea6dbdf4
FuratArch <128758064+FuratArch@users.noreply.github.com>
2023-03-23 16:46:25 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,15 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Publisher Privacy Policy</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Publisher Privacy Policy!" />
+        <Header title="Publisher Privacy Policy" />
         <p className="description">
           We do not collect any data from the users
-          You may NOT edit/copy/change/modify/publish our code/project/plugin with out our permission</code>
+          You may NOT edit/copy/change/modify/publish our code/project/plugin without our permission</code>
         </p>
       </main>
 
